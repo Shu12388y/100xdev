9eefee1d244975c0456267b7defca182a247cd73
shub12388y <spl.sp999@gmail.com>
2025-03-12 23:24:57 +0530

    added state

--- recoils/src/app/page.tsx
+++ recoils/src/app/page.tsx
@@ -3,13 +3,17 @@ import { countState, isEven } from "@/context/context";
 import { useRecoilValue, useSetRecoilState } from "recoil";
 
 export default function Home() {
-  const count = useRecoilValue(countState);
   const setCount = useSetRecoilState(countState);
   const isEvens = useRecoilValue(isEven);
+  
+  const Counter = () => {
+    const count = useRecoilValue(countState);
+    return <span>{count}</span>;
+  };
   return (
-    <div className="flex flex-col">
-      <button onClick={() => setCount((c) => c + 1)}>Increase</button>
-      <span>{count}</span>
+    <div className="flex flex-col items-center justify-center">
+      <button onClick={() => setCount((c) => c + 2)}>Increase</button>
+      <Counter/>
       <span>{isEvens}</span>
       <button onClick={() => setCount((c) => c - 1)}>Decrease</button>
     </div>
--- rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-/home/shubhham/Desktop/100xdev/rust/target/debug/rust: /home/shubhham/Desktop/100xdev/rust/src/main.rs
